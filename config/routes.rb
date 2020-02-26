c7cef87ff6edc976dcf579e382eb88d15fff099c
Pignataro67 <Pignataro67@gmail.com>
2020-02-26 08:31:55 -0800

    improving routes, trying geocoder func., using React mapbox

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  #scope(:path => '/api') do
-    get '/search_start/:input', to: 'search#search_start'
-  #end
+  scope(:path => '/RailsApi') do
+    get '/search_locations/:input', to: 'search#search_locations'
+    get '/confirm_route/convert_lat_long/:location', to: 'confirm_route#get_lat_long'
+    get '/confirm_route/mapbox', to: 'confirm_route#mapbox'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
