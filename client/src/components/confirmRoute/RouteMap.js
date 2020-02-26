0f3aef3e6f735cf9abe191f85325f2492f4d0b5e
Pignataro67 <Pignataro67@gmail.com>
2020-02-26 08:31:49 -0800

    improving routes, trying geocoder func., using React mapbox

--- client/src/components/confirmRoute/RouteMap.js
+++ client/src/components/confirmRoute/RouteMap.js
@@ -3,7 +3,53 @@ import ReactMapboxGl, { Layer, Marker } from "react-mapbox-gl";
 import { Icon } from 'semantic-ui-react';
 
 class RouteMap extends Component {
-    
+  state = {
+    Map: null
+  }
+
+  componentDidUpdate(prevProps){
+    if(!prevProps.mapboxKey && this.props.mapboxKey){
+      this.setState({
+        Map: ReactMapboxGl({
+          accessToken: this.props.mapboxKey
+        })
+      })
+    }
+  }
+
+  componentDidMount(){
+    if(this.props.mapboxKey){
+      this.setState({
+        Map: ReactMapboxGl({
+          accessToken: this.props.mapboxKey
+        })
+      })
+    } 
+  }
+
+  render() {
+    const {Map} = this.state
+    return Map? (
+    <Map
+      style="mapbox://styles/mapbox/streets-v9"
+      center={this.props.startLngLat}
+      containerStyle={{
+        position: 'relative',
+        height: "300px",
+        width: "400px",
+      }}>
+    <Layer
+      type="symbol"
+      id="marker"
+      layout={{
+          'icon-image': 'star-15',
+          'icon-allow-overlap': true }}>
+    <Marker coordinates={this.props.startLngLat}/>
+    <Marker coordinates={this.props.destinationLngLat}/>
+    </Layer>
+    </Map>
+    ): null
+  }
 }
 
 export default RouteMap;
--- client/src/reducers/rootReducer.js
+++ client/src/reducers/rootReducer.js
@@ -4,13 +4,33 @@ export default function rootReducer(state = {
   isFetchingStartingLocation: false,
   isFetchingDestination: false,
   suggestedStartingLocations: [],
-  suggestedDestinations: []
+  suggestedDestinations: [],
+  isConvertingStartLatLong: false,
+  isConvertingDestinationLatLong: false,
+  startLngLat: '',
+  destinationLngLat: ''
   }, action) {
   switch(action.type) {
+    case 'FETCHING_MAPBOX_KEY':
+      return {...state, isFetchingMapboxKey: true}
+    case "ADD_MAPBOX_KEY_TO_STATE":
+      return {...state, isFetchignMapboxKey: false, mapboxKey: action.key}
     case "FETCHING_SUGGESTED_START_LOCATIONS":
       return {...state, isFetchingStartingLocation: true, suggestedStartingLocations: []}
     case "DISPLAY_START_LOCATIONS":
       return {...state, isFetchingStartingLocation: false, suggestedStartingLocations: action.suggestedStartingLocations}
+    case "FETCHING_SUGGESTED_DESTINATIONS":
+      return {...state, isFetchingDestination: true, suggestedDestinations: []}
+    case "DISPLAY_DESTINATIONS":
+      return {...state, isFetchingDestination: false, suggestedDestinations: action.suggestedDestinations}
+    case "CONVERTING_START_LAT_LONG":
+      return {...state, isConvertingStartLatLong: true}
+    case "RETRIEVE_START_LAT_LONG":
+      return {...state, isConvertingStartLatLong: false, startLngLat: action.coordinates}
+    case "CONVERTING_DESTINATION_LAT_LONG":
+      return {...state, isConvertingDestinationLatLong: true}
+    case "RETRIEVE_DESTINATION_LAT_LONG":
+      return {...state, isConvertingDestinationLatLong: false, destinationLngLat: action.coordinates }
     default:
       return state
     }
