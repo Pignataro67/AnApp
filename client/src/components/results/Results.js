1de796cd0bdf0f9b2b3db6d308a62c2bb4361d63
Pignataro67 <Pignataro67@gmail.com>
2020-02-29 09:57:08 -0800

    adding results comp.

---
+++ client/src/components/results/Results.js
@@ -0,0 +1,12 @@
+import React, { Component} from 'react';
+import Result from './Result';
+
+class Results extends Component {
+
+  render(){
+  return(
+    <Result /> 
+  )};
+}
+
+export default Results;
